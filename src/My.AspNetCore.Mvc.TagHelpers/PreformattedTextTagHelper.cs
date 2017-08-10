0b3f0384f8c0601f6266567e1e5141cc8dfcb659
hishamco <hishamco_2007@yahoo.com>
2017-08-10 20:36:05 +0300

    Add PreformattedTextTagHelper

---
+++ src/My.AspNetCore.Mvc.TagHelpers/PreformattedTextTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace My.AspNetCore.Mvc.TagHelpers
+{
+    [HtmlTargetElement("pre")]
+    public class PreformattedTextTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var content = await output.GetChildContentAsync();
+            output.Content.SetHtmlContent(HtmlEncoder.Default.Encode(content.GetContent()));
+        }
+    }
+}
